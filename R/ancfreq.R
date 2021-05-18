ca0dfd90bf85ad3d8517f08f1ae6f1a6e94f9e9b
lycium0605 <51844050+lycium0605@users.noreply.github.com>
2021-05-18 23:00:59 +0800

    Adding warning / error message

--- R/ancfreq.R
+++ R/ancfreq.R
@@ -17,58 +17,72 @@ ancfreq<-function(inputdir_dip="missing",pop1_dip="missing",pop2_dip="missing",
                   outputdir){
 
   #Check input
+  flag=0
+
   if(inputdir_dip!='missing'){
     if(pop1_dip=='missing'){
       print("Please provide input for diploid reference population 1.")
-      #quit()
+      flag=1
     }
-    if(pop2_dip=='missing'){
+    else if(pop2_dip=='missing'){
       print("Please provide input for diploid reference population 2.")
-      #quit()
+      flag=1
     }
+    else{
     input=file(inputdir_dip,'r')
     line=unlist(strsplit(readLines(input,n=1),split = ' '))
     #print(line)
     indnum_dip=(length(line)-2)/3
-    print(paste("Finding",indnum_dip,"individuals in the file"))
-    close(input)
-    outdip=paste(outputdir,'_dip',sep = '')
-  }
-  if(inputdir_hap!='missing'){
-    if(pop1_hap=='missing'){
-      print("Please provide input for haploid reference population 1.")
-      #quit()
+    if(floor(indnum_dip)!=indnum_dip || indnum_dip < 1){
+      print(paste("Unexpectedly finding",indnum_dip,"individuals in the file,
+                  please double check your input format"))
+      flag = 1
     }
-    if(pop2_hap=='missing'){
-      print("Please provide input for haploid reference population 2.")
-      #quit()
+    else{
+      print(paste("Finding",indnum_dip,"individuals in the file"))
+      close(input)
+      outdip=paste(outputdir,'_dip',sep = '')
     }
-    input=file(inputdir_hap,'r')
-    line=unlist(strsplit(readLines(input,n=1),split = ' '))
-    indnum_hap=(length(line)-2)/2
-    close(input)
-    outhap=paste(outputdir,'_hap',sep = '')
+    }
   }
+  if(flag==0){
+    if(inputdir_hap!='missing'){
+      if(pop1_hap=='missing'){
+        print("Please provide input for haploid reference population 1.")
+        flag=1
+      }
+      else if(pop2_hap=='missing'){
+        print("Please provide input for haploid reference population 2.")
+        flag=1
+      }
+      else{
+        input=file(inputdir_hap,'r')
+        line=unlist(strsplit(readLines(input,n=1),split = ' '))
+        indnum_hap=(length(line)-2)/2
+        close(input)
+        outhap=paste(outputdir,'_hap',sep = '')
+      }
+    }
 
-  #Do ancestral allele frequency calculation
-  if(inputdir_dip!="missing"&&inputdir_hap!="missing"){
-    print('Generating ancestral allele frequency for diploid, haploid data and merge them.')
-    outmerge=paste(outputdir,'_merge')
-    #int n, int type, std::string pop1, std::string pop2, std::string input, std::string output
-    #ancfreq_c(n=indnum_dip,type=2,pop1=pop1_dip,pop2=pop2_dip,input=inputdir_dip,output=outdip)
-    #ancfreq_c(n=indnum_hap,type=1,pop1=pop1_hap,pop2=pop2_hap,input=inputdir_hap,output=outhap)
-  }
-  else if(inputdir_dip!='missing'){
-    print('Generating ancestral allele frequency for diploid data only.')
-    ancfreq_c(n=indnum_dip,type=2,pop1=pop1_dip,pop2=pop2_dip,input=inputdir_dip,output=outdip)
-  }
-  else if(inputdir_hap!='missing'){
-    print('Generating ancestral allele frequency for haploid data only.')
-    #ancfreq_c(n=indnum_hap,type=1,pop1=pop1_hap,pop2=pop2_hap,input=inputdir_hap,output=outhap)
-  }
-  else{
-    print("The input can't be missing for both types.")
-    #quit()
+    #Do ancestral allele frequency calculation
+    if(inputdir_dip!="missing"&&inputdir_hap!="missing"){
+      print('Generating ancestral allele frequency for diploid, haploid data and merge them.')
+      outmerge=paste(outputdir,'_merge')
+      #int n, int type, std::string pop1, std::string pop2, std::string input, std::string output
+      ancfreq_c(n=indnum_dip,type=2,pop1=pop1_dip,pop2=pop2_dip,input=inputdir_dip,output=outdip)
+      ancfreq_c(n=indnum_hap,type=1,pop1=pop1_hap,pop2=pop2_hap,input=inputdir_hap,output=outhap)
+    }
+    else if(inputdir_dip!='missing'){
+      print('Generating ancestral allele frequency for diploid data only.')
+      ancfreq_c(n=indnum_dip,type=2,pop1=pop1_dip,pop2=pop2_dip,input=inputdir_dip,output=outdip)
+    }
+    else if(inputdir_hap!='missing'){
+      print('Generating ancestral allele frequency for haploid data only.')
+      ancfreq_c(n=indnum_hap,type=1,pop1=pop1_hap,pop2=pop2_hap,input=inputdir_hap,output=outhap)
+    }
+    else{
+      print("The input can't be missing for both types.")
+    }
   }
 }
 
