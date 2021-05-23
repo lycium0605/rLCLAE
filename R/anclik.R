087fb23599c1733cf0b6a609766aaef19191ef9d
lycium0605 <51844050+lycium0605@users.noreply.github.com>
2021-05-23 16:35:34 +0800

    Adding suffix and adding all option
    Allowing using "all" to automatically run for all individuals detected, and adding a suffix of the test individual incdex.

--- R/anclik.R
+++ R/anclik.R
@@ -7,13 +7,13 @@
 #' @param ancfreqdir ancestral allele frequency file
 #' @param outputdir ancestral state estimation
 #' @param type 'hap' or 'dip'
-#' @param test the individual number to be estimated
+#' @param test can be 'all' or a numeric vector containing the individual number to be estimated
 #'
 #' @return nothing
 #' @export
 #'
 # @examples
-anclik<-function(genodir,ancfreqdir,outputdir,type='dip',test){
+anclik<-function(genodir,ancfreqdir,outputdir,type='dip',test='all'){
   flag=0
   if(!file.exists(genodir)){
     print("The genotype likelihood file you provided does not exist. Please check.")
@@ -49,16 +49,32 @@ anclik<-function(genodir,ancfreqdir,outputdir,type='dip',test){
   }
 
   if(flag==0){
-    anclik_c(sum=indnum,type=typenum,testid=test,
-             genolik=genodir,ancfreq=ancfreqdir,output=outputdir)
+    if(length(test==1)&&test=='all'){
+      for(i in 1:indnum){
+        outputdir_=paste(outputdir,i,sep = '_')
+        anclik_c(sum=indnum,type=typenum,testid=i,
+                 genolik=genodir,ancfreq=ancfreqdir,output=outputdir_)
+      }
+    }
+    else{
+      for(i in test){
+        i_=as.numeric(i)
+        if(i_<=indnum){
+          outputdir_=paste(outputdir,i_,sep = '_')
+          anclik_c(sum=indnum,type=typenum,testid=i_,
+                   genolik=genodir,ancfreq=ancfreqdir,output=outputdir_)
+        }
+      }
+    }
+
   }
 
 }
 
-#' test_anclik
+#' test_ancli m k
 #'
-#' @return nothing
-#' @export
+ #' @return nothing
+# @export
 #'
 # @examples
 test_anclik<-function(){
@@ -67,5 +83,5 @@ test_anclik<-function(){
   input2=paste(data,"ancfreq_dip",sep = '')
   output_=paste(data,"anclik_R",sep = '')
   anclik(genodir = input1,ancfreqdir = input2,outputdir =output_,
-         type = 'dip',test = 3 )
+         type = 'dip',test = 1 )
 }
