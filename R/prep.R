eac7760d337e5be7f26d7f848b7728b4be215474
lycium0605 <51844050+lycium0605@users.noreply.github.com>
2021-05-24 15:37:08 +0800

    Update documentation

--- R/prep.R
+++ R/prep.R
@@ -1,4 +1,14 @@
 
+#' preprocess
+#' A function to preprocess the vcf file
+#'
+#' @param inputdir dir to orginial vcf file
+#' @param outputdir dir to store cleaned_up vcf file
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples preprocess("\data\vcf","\data\vcf_clean")
 preprocess<-function(inputdir,outputdir){
   clean="sed 's/|/\\//g' | sed '/^#/d' | cut -f 1,2,10- | sed -e 's/:/ /g' | sed -e 's/\\./999/g'"
   command_=paste(sep='','cat ',inputdir, ' | ',clean,'>',outputdir)
@@ -6,6 +16,14 @@ preprocess<-function(inputdir,outputdir){
   inputcheck(outputdir)
 }
 
+#' inputcheck
+#' A function to check the format of the input cleaned-up vcf file
+#' @param inputdir dir to clean vcf
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples inputcheck('\data\cleanvcf')
 inputcheck<-function(inputdir){
   #Check characters
   com = '| grep [^0-9,[:space:]/] > /dev/null&& echo "Unexpected character, please double check your input" || echo "Pass character test"'
