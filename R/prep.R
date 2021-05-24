3600b9ec9f0f355f991680d4ad56c72e6cfdadd2
lycium0605 <51844050+lycium0605@users.noreply.github.com>
2021-05-24 10:44:53 +0800

    Create preprocessing and input check functions

---
+++ R/prep.R
@@ -0,0 +1,32 @@
+
+preprocess<-function(inputdir,outputdir){
+  clean="sed 's/|/\\//g' | sed '/^#/d' | cut -f 1,2,10- | sed -e 's/:/ /g' | sed -e 's/\\./999/g'"
+  command_=paste(sep='','cat ',inputdir, ' | ',clean,'>',outputdir)
+  system(command=command_,intern = FALSE)
+  inputcheck(outputdir)
+}
+
+inputcheck<-function(inputdir){
+  #Check characters
+  com = '| grep [^0-9,[:space:]/] > /dev/null&& echo "Unexpected character, please double check your input" || echo "Pass character test"'
+  command_ = paste(sep=' ', 'cat',inputdir,com)
+  system(command = command_,intern = TRUE) -> charcheck
+  print(charcheck)
+  if(charcheck=="Pass character test"){
+    #Check individual number
+    input=file(inputdir,'r')
+    line=unlist(strsplit(readLines(input,n=1),split = '\t'))
+    indnum=length(line)-2
+    close(input)
+    print(paste(sep=' ',"Finding",indnum,"individuals from this vcf file."))
+
+    #Check chromosomes
+    x<-get_chrlist(inputdir)
+    chr<-x[seq(2,length(x),2)]
+    rep<-as.numeric(x[seq(1,length(x),2)])
+    print(paste(sep = ' ',"Finding",length(chr),"unique chromosomes in this vcf file, Including:"))
+    for(i in 1:length(chr)){
+      print(paste(rep[i],"snps in",chr[i]))
+  }
+  }
+  }
